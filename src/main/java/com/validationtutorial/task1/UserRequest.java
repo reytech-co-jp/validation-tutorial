66dcb079831193defc6295189e6213801a3b5935
renangton <puccho0427@gmail.com>
2023-11-26 21:11:31 +0900

    minとmaxで動的にメッセージが生成されるように修正、デフォルトコンストラクタを追加

--- src/main/java/com/validationtutorial/task1/UserRequest.java
+++ src/main/java/com/validationtutorial/task1/UserRequest.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Size;
 
 public class UserRequest {
     @NotBlank(message = "ユーザー名を入力してください")
-    @Size(min = 3, max = 20, message = "ユーザー名は3文字以上20文字以下である必要があります")
+    @Size(min = 3, max = 20, message = "ユーザー名は{min}文字以上{max}文字以下である必要があります")
     private String username;
 
     @NotBlank(message = "パスワードを入力してください")
-    @Size(min = 8, max = 30, message = "パスワードは8文字以上30文字以下である必要があります")
+    @Size(min = 8, max = 30, message = "パスワードは{min}文字以上{max}文字以下である必要があります")
     private String password;
 
     private String confirmPassword;
@@ -19,6 +19,9 @@ public class UserRequest {
         this.username = username;
         this.password = password;
         this.confirmPassword = confirmPassword;
+    }
+
+    public UserRequest() {
     }
 
     public String getUsername() {
